98a3e95203a4f1d75030cc25463c0701e0f306a1
Sirjanpreet Singh <sirjanpreet.singh@outlook.com>
2023-10-14 12:05:57 -0700

    index

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import os
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
