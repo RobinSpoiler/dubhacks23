01ce7690dbb88105c979fa68f87f95473bd2245e
Sirjanpreet Singh <sirjanpreet.singh@outlook.com>
2023-10-15 09:52:49 -0700

    removed cs50

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-from cs50 import SQL
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 from helpers import login_required
